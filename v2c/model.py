49910eb57d3d52bca33fc80a95e99b187bea3e40
Moloy Das <modas@ucsd.edu>
2024-05-30 14:06:49 -0700

    changed transformer param

--- v2c/model.py
+++ v2c/model.py
@@ -7,6 +7,7 @@ from torch import Tensor
 import torch.nn.functional as F
 from torchvision import models
 from v2c.backbone import resnet
+import sys
 
 # ----------------------------------------
 # Functions for Video Feature Extraction
@@ -61,10 +62,11 @@ class CNNWrapper(nn.Module):
 # ----------------------------------------
 # Transformer Encoder and Decoder for V2CNet
 # ----------------------------------------
-
 class PositionalEncoding(nn.Module):
 
-    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 10):
+    def __init__(self, d_model: int, 
+                 dropout: float = 0.1, 
+                 max_len: int = 10):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
 
@@ -73,6 +75,9 @@ class PositionalEncoding(nn.Module):
         pe = torch.zeros(max_len, d_model)
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(-2)
+
+        self.dropout = nn.Dropout(dropout)
         self.register_buffer('pe', pe)
 
     def forward(self, x: Tensor) -> Tensor:
@@ -80,7 +85,7 @@ class PositionalEncoding(nn.Module):
         Arguments:
             x: Tensor, shape ``[batch_size, seq_len, embedding_dim]``
         """
-        x = x + self.pe[:x.size(1)]
+        x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
 
 class TransformerEncoder(nn.Module):
@@ -93,8 +98,7 @@ class TransformerEncoder(nn.Module):
                                 nn.TransformerEncoderLayer( config.N_FEATURES, 
                                                             config.N_HEAD, 
                                                             dim_feedforward=dim_feedforward, 
-                                                            dropout=dropout,
-                                                            batch_first=True), config.N_ENCODING_LAYER)
+                                                            dropout=dropout), config.N_ENCODING_LAYER)
 
         self.linear = nn.Linear(config.N_FEATURES, config.EMBED_SIZE)
 
@@ -114,14 +118,13 @@ class TransformerDecoder(nn.Module):
         self.decoder = nn.TransformerDecoder(nn.TransformerDecoderLayer(config.EMBED_SIZE, 
                                                                             config.N_HEAD, 
                                                                             dim_feedforward, 
-                                                                            dropout,
-                                                                            batch_first=True), config.N_DECODING_LAYER)
+                                                                            dropout), config.N_DECODING_LAYER)
 
-    def forward(self, target, enc_out, target_mask):
+    def forward(self, target, enc_out, target_mask, target_mask_padding):
         # Decode features and generate captions using Transformer
         x = self.embedding(target)
         x = self.position_embedding(x)
-        output = self.decoder(x, enc_out, tgt_mask=target_mask)
+        output = self.decoder(x, enc_out, tgt_mask=target_mask, tgt_key_padding_mask=target_mask_padding)
         return output
 
 class TransformerV2C(nn.Module):
@@ -131,14 +134,25 @@ class TransformerV2C(nn.Module):
         self.encoder = TransformerEncoder(config, d_hid, dropout)
         self.decoder = TransformerDecoder(config, d_hid, dropout)
         self.linear = nn.Linear(config.EMBED_SIZE, config.VOCAB_SIZE)
-        self.softmax = nn.LogSoftmax(dim=-1)
   
-    def forward(self, v_feat, tgt, tgt_mask):
+    def forward(self, v_feat, tgt, tgt_mask, tgt_mask_padding):
         enc_out = self.encoder(v_feat)
-        output = self.decoder(tgt, enc_out, tgt_mask)
+        output = self.decoder(tgt, enc_out, tgt_mask, tgt_mask_padding)
         output = self.linear(output)
-        output = self.softmax(output)
         return output
+
+def generate_square_subsequent_mask(sz, device):
+    mask = (torch.triu(torch.ones((sz, sz), device=device)) == 1).transpose(0, 1)
+    mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+    return mask
+
+def create_mask(tgt):
+    tgt_seq_len = tgt.shape[0]
+
+    tgt_mask = generate_square_subsequent_mask(tgt_seq_len, tgt.device)
+
+    tgt_padding_mask = (tgt == 0).transpose(0, 1)
+    return tgt_mask, tgt_padding_mask
 
 # ----------------------------------------
 # Functions for V2CNet
@@ -149,8 +163,8 @@ class CommandLoss(nn.Module):
     def __init__(self, 
                  ignore_index=0):
         super(CommandLoss, self).__init__()
-        self.cross_entropy = nn.NLLLoss(reduction='sum', 
-                                        ignore_index=ignore_index)
+        self.cross_entropy = nn.CrossEntropyLoss(reduction='sum', 
+                                                ignore_index=ignore_index)
 
     def forward(self, 
                 input, 
@@ -177,8 +191,6 @@ class Video2Command():
         self.loss_objective.to(self.device)
 
         # Setup parameters and optimizer
-        # self.params = list(self.video_encoder.parameters()) + \
-        #               list(self.command_decoder.parameters())
         self.params = list(self.transformerV2C.parameters())
         self.optimizer = torch.optim.Adam(self.params, 
                                           lr=self.config.LEARNING_RATE)
@@ -199,41 +211,23 @@ class Video2Command():
             # Zero the parameter gradients
             self.optimizer.zero_grad()
 
+            Xv = Xv.permute(1, 0, 2)   # (B, L, C) -> (L, B, C)
+            S = S.permute(1, 0)   # (B, L) -> (L, B)
+
+            tgt_input = S[:-1, :]
+            tgt_output = S[1:, :]
+
             # Get target_mask for Transformer
-            S_mask = S != 0
-            tgt_mask = S_mask.unsqueeze(1).unsqueeze(3)
-            nopeak_mask = (1 - torch.triu(torch.ones(self.config.N_HEAD, self.config.MAXLEN, self.config.MAXLEN), diagonal=1)).bool()
-            nopeak_mask = nopeak_mask.to(self.device)
-            tgt_mask = tgt_mask & nopeak_mask
-            tgt_mask = tgt_mask.view(-1, self.config.MAXLEN, self.config.MAXLEN)
-            tgt_mask = tgt_mask.to(self.device)
+            tgt_mask, tgt_padding_mask = create_mask(tgt_input)
 
             # Get captions with Transformer
-            probs = self.transformerV2C(Xv, S, tgt_mask=tgt_mask)
+            logits = self.transformerV2C(Xv, tgt_input, 
+                                         tgt_mask=tgt_mask, 
+                                         tgt_mask_padding=tgt_padding_mask)
+            loss = self.loss_objective(logits.reshape(-1, logits.shape[-1]), 
+                                       tgt_output.reshape(-1))
 
-            # Xv = self.video_encoder(Xv)
-            # print(S)
-            # # Xv, states = self.video_encoder(Xv)
-
-            # # Calculate mask against zero-padding
-            # S_mask = S != 0
-
-            # # Teacher forcing for caption decoder (replace this if needed)
-            # for timestep in range(self.config.MAXLEN - 1):
-            #     # Xs = S[:, timestep]
-            #     # probs, states = self.command_decoder(Xs, Xv, tgt_mask=S_mask[:, :timestep+1])  # Use previous captions for mask
-
-            #     Xs = S[:, timestep]
-            #     # Modify causal mask to include current timestep
-            #     attn_mask = torch.triu(torch.ones((Xs.shape[0], Xs.shape[0]), dtype=torch.bool)).to(self.device)
-            #     attn_mask[:, :timestep+1] = 1  # Include current timestep for teacher forcing
-            #     probs, states = self.command_decoder(Xs, Xv, tgt_mask=attn_mask)
-
-            #     # Calculate loss per word
-            #     loss += self.loss_objective(probs, S[:, timestep+1])
-            loss = self.loss_objective(probs[:,:-1,:].reshape(-1, probs.shape[-1]), S[:, 1:].reshape(-1))
-
-            loss = loss / S_mask.sum()     # Loss per word
+            # loss = loss / S_mask.sum()     # Loss per word
 
             # Gradient backward
             loss.backward()
@@ -241,8 +235,6 @@ class Video2Command():
             return loss
 
         # Training epochs
-        # self.video_encoder.train()
-        # self.command_decoder.train()
         self.transformerV2C.train()
         for epoch in range(self.config.NUM_EPOCHS):
             total_loss = 0.0
@@ -315,8 +307,6 @@ class Video2Command():
         """
         # Save the current checkpoint
         torch.save({
-                    # 'VideoEncoder_state_dict': self.video_encoder.state_dict(),
-                    # 'CommandDecoder_state_dict': self.command_decoder.state_dict(),
                     'transformerV2C_state_dict': self.transformerV2C.state_dict(),
                     'optimizer_state_dict': self.optimizer.state_dict(),
                     }, os.path.join(self.config.CHECKPOINT_PATH, 'saved', 'v2c_epoch_{}.pth'.format(epoch)))
@@ -328,8 +318,6 @@ class Video2Command():
         """
         print('Loading...')
         checkpoint = torch.load(save_path)
-        # self.video_encoder.load_state_dict(checkpoint['VideoEncoder_state_dict'])
-        # self.command_decoder.load_state_dict(checkpoint['CommandDecoder_state_dict'])
         self.transformerV2C.load_state_dict(checkpoint['transformerV2C_state_dict'])
         self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         print('Model loaded.')
